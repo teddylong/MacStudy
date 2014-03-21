74742b1ab5b915dac95c7c15885883ffb4826aa5
Ctrip <Ctrip@CtripdeMac.local>
2014-03-20 22:13:38 -0700

    Initial Commit

---
+++ TestTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TestTest
+//
+//  Created by Ctrip on 14-3-20.
+//  Copyright (c) 2014年 Ctrip. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
